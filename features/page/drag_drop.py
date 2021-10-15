cb12a250daeb63723174eccdaeb9206fc058f11a
nadiakbiaer <nadia_kbaier@outlook.fr>
2021-10-15 11:20:18 +0200

    req

--- features/page/drag_drop.py
+++ features/page/drag_drop.py
@@ -1,3 +1,6 @@
+from os import minor
+
+import allure
 from Browser import Browser
 from selenium.webdriver import ActionChains
 
@@ -9,6 +12,7 @@ class drag_drop(Browser):
     def url(self, link):
         self.driver.get(link)
 
+    @allure.severity_level(minor)
     def drag(self):
         source1 = self.driver.find_element_by_id(objectTodrag)
         target1 = self.driver.find_element_by_id(objectTodrop)
